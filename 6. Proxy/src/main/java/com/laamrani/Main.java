29f9bba2bcc3b80e4945b01c269fdb81ae4b6f4f
Oumaima-LAAMRANI <oumaima.laamrani.2002@gmail.com>
2023-10-28 02:42:34 +0200

    [Design Pattern Proxy]

---
+++ 6. Proxy/src/main/java/com/laamrani/Main.java
@@ -0,0 +1,9 @@
+package com.laamrani;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client=new Client();
+        client.setStandard(new Proxy());
+        client.process();
+    }
+}
